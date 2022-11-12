7647523b713ad99236a96fce5ea027ea81c784f4
Harshraj Todake <djharshraj@gmail.com>
2022-11-12 22:11:46 +0530

    assignment-completed

---
+++ src/app/SuccessAlert/SuccessAlert.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-SuccessAlert',
+  templateUrl: './SuccessAlert.component.html',
+  styleUrls: ['./SuccessAlert.component.css']
+})
+export class SuccessAlertComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
